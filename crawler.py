f70d3f510e24e9fd11ff7ebd5cf248d059795de3
ateyamusungu <32094557+ateyamusungu@users.noreply.github.com>
2017-09-28 15:27:22 +0300

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -2,12 +2,12 @@ from bs4 import BeautifulSoup
 
 import urllib
 
-url= "http://www.strathmore.edu"
+url = "http://www.strathmore.edu"
 
 
-data=urllib.urlopen(url).read()
+data = urllib.urlopen(url).read()
 
-soup=BeautifulSoup(data)
+soup = BeautifulSoup(data)
 
 for link in soup.find_all('a', href=True);
 
