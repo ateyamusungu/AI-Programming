20ad74da6ba0a37a2ff88b50e19978e2ea4aff0c
ateyamusungu <32094557+ateyamusungu@users.noreply.github.com>
2017-09-28 15:24:27 +0300

    Create crawler.py

---
+++ crawler.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+
+import urllib
+
+url= "http://www.strathmore.edu"
+
+
+data=urllib.urlopen(url).read()
+
+soup=BeautifulSoup(data)
+
+for link in soup.find_all('a', href=True);
+
+print(link.get('href'))
